5aa41ac7462b65549b97038f18a541cc9279891f
E_github-AD:24-25 <eli.gomezveloz@gmail.com>
2024-11-13 07:14:46 +0100

    Cambios configuraciones

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-=======
 /* eslint-disable quote-props */
 /* eslint-disable no-dupe-keys */
->>>>>>> 7645d27a3c973849c08e9797d7a1608d46e10eeb
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-<<<<<<< HEAD
-  },
-  extends: 'airbnb-base',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {},
-  plugins: ['jest'],
-  env: {
-    'jest/globals': true,
-=======
     'jest/globals': true,
   },
   extends: 'airbnb-base',
@@ -30,6 +15,5 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'import/extensions': ['error', 'ignorePackages', { 'js': 'never' }],
->>>>>>> 7645d27a3c973849c08e9797d7a1608d46e10eeb
   },
 };
