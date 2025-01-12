e4e290334f93cfe40eabd67fbeba0c13da658110
E_github-AD:24-25 <eli.gomezveloz@gmail.com>
2025-01-12 10:18:40 +0100

    Actualizar .gitignore y eliminar archivos rastreados

--- databases/clase1/compose-postgres/tienda-deportes/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('tienda_deportes', 'postgres', 'changeme', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
--- databases/plataforma_arte_local/connection.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize, DataTypes } = require('sequelize');
-
-const sequelize = new Sequelize('plataforma_arte_local', 'postgres', 'changeme', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-module.exports = sequelize;
